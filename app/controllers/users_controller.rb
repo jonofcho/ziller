795194da66872ca543a08ec5a64563eea0518f03
Jonathan Cho <jonofcho@gmail.com>
2018-01-28 19:17:25 -0800

    create lacreatee index layout add assets, non-responsive at the moment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
   def index
+    today = Date::DAYNAMES[Date.today.wday]
+    puts today
+    @date = today.downcase
+    if admin_signed_in?
+      @verify = true
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :admins, path: 'admins'
   resources :collections
   resources :products
   resources :users
 
+  devise_scope :admin do
+    get 'sign_in', to: 'devise/sessions#new'
+    get 'sign_out', to: 'devise/sessions#destroy'
+  end
+  # devise_for :admins, path: 'admins'
   root to: "users#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
